01d221d7b06567eba373706fde4341dfee81a06a
Yoann Anezin <yoann.anezin@artelys.com>
2025-06-04 17:49:50 +0200

    feat(solver): add a first version of the Resilient LF with Reactiv Limits Constraints Model implemented with Knitro Signed-off-by: Yoann Anezin <yoann.anezin@artelys.com>

--- src/main/java/com/powsybl/openloadflow/knitro/solver/KnitroSolverParameters.java
+++ src/main/java/com/powsybl/openloadflow/knitro/solver/KnitroSolverParameters.java
@@ -295,6 +295,7 @@ public class KnitroSolverParameters implements AcSolverParameters {
 
     public enum SolverType {
         STANDARD,
-        RELAXED
+        RELAXED,
+        REACTIVLIMITS,
     }
 }
