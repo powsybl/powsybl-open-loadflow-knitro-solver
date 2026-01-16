c5a7d575ffad578e833fe3b9090839c5eaf17259
p-arvy <pierre.arvy@artelys.com>
2026-01-16 18:20:17 +0100

    clean TU
    Signed-off-by: p-arvy <pierre.arvy@artelys.com>

--- src/main/java/com/powsybl/openloadflow/knitro/solver/AbstractKnitroSolver.java
+++ src/main/java/com/powsybl/openloadflow/knitro/solver/AbstractKnitroSolver.java
@@ -143,7 +143,7 @@ public abstract class AbstractKnitroSolver extends AbstractAcSolver {
      * @param solver The Knitro solver instance.
      * @return The solution.
      */
-    protected KNSolution getSolution(KNSolver solver) {
+    protected KNSolution getSolution(KNSolver solver) throws KNException {
         return solver.getSolution();
     }
 
@@ -173,6 +173,8 @@ public abstract class AbstractKnitroSolver extends AbstractAcSolver {
 
         try {
             instance = createKnitroProblem(voltageInitializer);
+        } catch (PowsyblException e) {
+            throw e;
         } catch (Exception e) {
             throw new PowsyblException("Exception while trying to build Knitro Problem", e);
         }
