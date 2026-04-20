bc16af4c062f635b876b32994dcd51b633c90fdc
Salomé Lavine <salome.lavine@artelys.com>
2026-04-20 16:56:45 +0200

    Refactor (deleted code relating to private data) in NetworkProviders and ResilientAcLoadFlowPerturbationTest
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>

--- src/test/java/com/powsybl/openloadflow/knitro/solver/NetworkProviders.java
+++ src/test/java/com/powsybl/openloadflow/knitro/solver/NetworkProviders.java
@@ -3,13 +3,9 @@ package com.powsybl.openloadflow.knitro.solver;
 
 import com.powsybl.ieeecdf.converter.IeeeCdfNetworkFactory;
 import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.TopologyKind;
-import com.powsybl.iidm.network.VoltageLevel;
 import com.powsybl.iidm.serde.XMLExporter;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Properties;
 import java.util.stream.Stream;
 
@@ -19,13 +15,7 @@ import java.util.stream.Stream;
  */
 public final class NetworkProviders {
     public static final String DATA_DIR = "data";
-    public static final String CONFIDENTIAL_DATA_DIR = "data/data_confidential/";
-    public static final String CONFIDENTIAL_DATA_DIR_BUS_BREAKER = "../../HU_bb/";
-    public static final String CONFIDENTIAL_DATA_DIR_NODE_BREAKER = "../../HU_nb/";
     public static final String DEFAULT_OUTPUT_DIR = "./Outputs/";
-    public static final String HU_INSTANCE = "20220226T2330Z_1D_002/init.xiidm";
-    public static final String ES_INSTANCE = "20250830T1330Z_1D_ES_006.xiidm";
-    public static final String TYNDP_INSTANCE = "CGM_TYNDP22.xiidm";
     public static final String RTE6515_INSTANCE = "rte6515.xiidm";
     public static final String RTE1888_INSTANCE = "rte1888.xiidm";
 
@@ -51,76 +41,11 @@ public final class NetworkProviders {
         );
     }
 
-    public static Stream<NetworkPair> provideHUNetworks(String dir) {
-//        Path baseDir = Path.of(dir, "HU_nb");
-        Path baseDir = Path.of(dir);
-        String initFileName = "init.xiidm";
-
-        try (Stream<Path> cases = Files.list(baseDir)) {
-            List<NetworkPair> networkPairs = cases.filter(Files::isDirectory)
-                    .map(subDir -> subDir.resolve(initFileName))
-                    .filter(Files::exists)
-                    .map(initPath -> {
-                        Network nrNetwork = Network.read(initPath).getNetwork();
-                        Network rknNetwork = Network.read(initPath).getNetwork();
-                        Network dcNetwork = Network.read(initPath).getNetwork();
-                        String name = initPath.getParent().getFileName().toString();
-                        return new NetworkPair(rknNetwork, nrNetwork, dcNetwork, name);
-                    })
-                    .toList();
-            return networkPairs.stream();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load HU real network cases", e);
-        }
-    }
-
-    public static Stream<NetworkPair> provideNodeBreakerHUNetworks() {
-        return provideHUNetworks(CONFIDENTIAL_DATA_DIR + "HU_nb");
-    }
-
-    public static Stream<NetworkPair> provideBusBreakerHUNetworks() {
-        return provideHUNetworks(CONFIDENTIAL_DATA_DIR + "HU_bb");
-    }
-
     public static void writeXML(Network network, String name) {
         Properties properties = new Properties();
         properties.put(XMLExporter.VERSION, "1.12");
         Path path = Path.of(DEFAULT_OUTPUT_DIR, name);
         network.write("XIIDM", properties, path);
-    }
-
-    public static void exportNetworkAsBusBreakerTopology(Path initPath, Path endPath) {
-        Network network = Network.read(initPath).getNetwork();
-        for (VoltageLevel vl : network.getVoltageLevels()) {
-            vl.convertToTopology(TopologyKind.BUS_BREAKER);
-        }
-        Properties exportParameters = new Properties();
-        exportParameters.put(XMLExporter.TOPOLOGY_LEVEL, "BUS_BREAKER");
-        network.write("XIIDM", exportParameters, endPath);
-    }
-
-    public static void convertNodeBreakerDataToBusBreaker(String nodeBreakerDir, String busBreakerDir, String initFileName) {
-        Path initRoot = Path.of(nodeBreakerDir);
-        Path endRoot = Path.of(busBreakerDir);
-
-        try (Stream<Path> pathStream = Files.list(initRoot)) {
-            pathStream.filter(Files::isDirectory)
-                    .map(subDir -> subDir.resolve(initFileName))
-                    .filter(Files::exists)
-                    .forEach(initFile -> {
-                        try {
-                            Path relativePath = initRoot.relativize(initFile);
-                            Path outputFile = endRoot.resolve(relativePath);
-                            Files.createDirectories(outputFile.getParent());
-                            exportNetworkAsBusBreakerTopology(initFile, outputFile);
-                        } catch (Exception e) {
-                            throw new RuntimeException("Failed to convert the files", e);
-                        }
-                    });
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load HU real network cases", e);
-        }
     }
 
     public record NetworkPair(Network rknNetwork, Network nrNetwork, Network dcNetwork, String baseFilename) {
--- src/test/java/com/powsybl/openloadflow/knitro/solver/ResilientAcLoadFlowPerturbationTest.java
+++ src/test/java/com/powsybl/openloadflow/knitro/solver/ResilientAcLoadFlowPerturbationTest.java
@@ -12,22 +12,13 @@ import com.powsybl.openloadflow.OpenLoadFlowProvider;
 import com.powsybl.openloadflow.dc.equations.DcApproximationType;
 import com.powsybl.openloadflow.network.SlackBusSelectionMode;
 import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
-
 import com.powsybl.openloadflow.knitro.solver.NetworkProviders.NetworkPair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.nio.file.Path;
-import static com.powsybl.openloadflow.knitro.solver.NetworkProviders.CONFIDENTIAL_DATA_DIR;
-//import static com.powsybl.openloadflow.knitro.solver.NetworkProviders.CONFIDENTIAL_DATA_DIR_BUS_BREAKER;
-import static com.powsybl.openloadflow.knitro.solver.NetworkProviders.HU_INSTANCE;
-import static com.powsybl.openloadflow.knitro.solver.NetworkProviders.ES_INSTANCE;
 
 /**
  * @author Martin Debouté {@literal <martin.deboute at artelys.com>}
@@ -37,7 +28,6 @@ public class ResilientAcLoadFlowPerturbationTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(ResilientAcLoadFlowPerturbationTest.class);
     private static final String RKN = "KNITRO";
     private static final String NR = "NEWTON_RAPHSON";
-    private static final String DC_TEST = "DC_TEST";
     private static final String VOLTAGE_PERTURBATION = "voltage-perturbation";
     private static final String ACTIVE_POWER_PERTURBATION = "active-perturbation";
     private static final String REACTIVE_POWER_PERTURBATION = "reactive-perturbation";
@@ -61,9 +51,7 @@ public class ResilientAcLoadFlowPerturbationTest {
         OpenLoadFlowParameters.create(parameters)
                     .setSlackBusSelectionMode(SlackBusSelectionMode.MOST_MESHED)
                     .setAcSolverType(solver);
-//        } else if (DC_TEST.equals(solver)) {
-//
-            // .setDcApproximationType(DcApproximationType.IGNORE_G); Should it be included to really compute a DC LF ?
+
         if (RKN.equals(solver)) {
             KnitroLoadFlowParameters knitroParams = new KnitroLoadFlowParameters();
             // Set the Knitro solver type to RELAXED
@@ -71,6 +59,17 @@ public class ResilientAcLoadFlowPerturbationTest {
             knitroParams.setKnitroSolverType(KnitroSolverParameters.SolverType.RELAXED);
             parameters.addExtension(KnitroLoadFlowParameters.class, knitroParams);
         }
+    }
+
+    private void configureDcSolver() {
+        LoadFlowParameters dcParameters = new LoadFlowParameters()
+                .setUseReactiveLimits(false)
+                .setDistributedSlack(false)
+                .setVoltageInitMode(LoadFlowParameters.VoltageInitMode.DC_VALUES);
+        OpenLoadFlowParameters.create(dcParameters)
+                .setSlackBusSelectionMode(SlackBusSelectionMode.MOST_MESHED)
+                .setAcSolverType(NR)
+                .setDcApproximationType(DcApproximationType.IGNORE_R);
     }
 
     private void compareResilience(Network rknNetwork, Network nrNetwork, Network dcNetwork, String baseFilename, String perturbationType) {
@@ -86,14 +85,7 @@ public class ResilientAcLoadFlowPerturbationTest {
         assumeFalse(isConvergedNR && !isFailedNR, baseFilename + ": NR should not converge");
 
         // DC Load Flow
-        LoadFlowParameters dcParameters = new LoadFlowParameters()
-                .setUseReactiveLimits(false)
-                .setDistributedSlack(false)
-                .setVoltageInitMode(LoadFlowParameters.VoltageInitMode.DC_VALUES);
-        OpenLoadFlowParameters.create(dcParameters)
-                .setSlackBusSelectionMode(SlackBusSelectionMode.MOST_MESHED)
-                .setAcSolverType(NR)
-                .setDcApproximationType(DcApproximationType.IGNORE_R);
+        configureDcSolver();
         LoadFlowResult resultDC = loadFlowRunner.run(dcNetwork, parameters);
         boolean isConvergedDC = resultDC.isFullyConverged();
         LOGGER.info("==== Test Information ====");
@@ -102,7 +94,6 @@ public class ResilientAcLoadFlowPerturbationTest {
         LOGGER.info("Network name : {}", baseFilename);
         //This line assures that the Losses values are computed and non null
         assertTrue(isConvergedDC, baseFilename + ": DC load flow should converge");
-        // If does not converges do Losses = aPgen ?
         this.losses = calculateDcLosses(dcNetwork); // Set the parameters losses so that the RKN RELAXED solver can acces it
         LOGGER.info("Calculated DC losses: {} MW", this.losses);
 
@@ -143,13 +134,11 @@ public class ResilientAcLoadFlowPerturbationTest {
     }
 
     private double calculateDcLosses(Network dcNetwork) {
-        // Computation donc on lines, should it also be on transfo ? Is it relevent ?
         double totalLosses = 0.0;
         for (Line line : dcNetwork.getLines()) {
             Terminal terminal1 = line.getTerminal1();
-            double p1 = terminal1.getP(); // unit ?? MW injected at terminal
-            // double p2 = terminal2.getP(); //units ? MW injected at the terminal
-            double r = line.getR(); // unit ? ohms
+            double p1 = terminal1.getP(); // MW injected at terminal
+            double r = line.getR(); // Ohms
             if (r == 0) {
                 continue;
             } else if (Double.isNaN(p1)) {
@@ -157,10 +146,7 @@ public class ResilientAcLoadFlowPerturbationTest {
                 continue;
             }
             double vnom1 = terminal1.getVoltageLevel().getNominalV(); // kV
-            //  double vnom2 = terminal2.getVoltageLevel().getNominalV(); //kV
             double loss = r * (Math.abs(p1) * Math.abs(p1)) / (vnom1 * vnom1);
-            //  exactLosses += p1 + p2; // MW, should be negative if losses
-            // LOGGER.info("Line {}: v1={} kV, P1={} MW, loses r*P12^2/vnom1^2 = {} MW",line.getId(), vnom1, p1, loss);
             totalLosses += loss;
         }
 
@@ -184,57 +170,6 @@ public class ResilientAcLoadFlowPerturbationTest {
         double alpha = 0.95;
 
         voltagePerturbationTest(rknNetwork, nrNetwork, dcNetwork, baseFilename, rPU, xPU, alpha);
-    }
-
-    @Test
-    void testVoltagePerturbationOnHUInstance() {
-        Path fileName = Path.of(CONFIDENTIAL_DATA_DIR + "HU_nb/", HU_INSTANCE);
-        Network nrNetwork = Network.read(fileName).getNetwork();
-        Network rknNetwork = Network.read(fileName).getNetwork();
-        Network dcNetwork = Network.read(fileName).getNetwork();
-
-        // Line Characteristics in per-unit
-        double rPU = 0.0;
-        double xPU = 1e-5;
-        // Voltage Mismatch
-        double alpha = 0.95;
-
-        voltagePerturbationTest(rknNetwork, nrNetwork, dcNetwork, "HU", rPU, xPU, alpha);
-    }
-
-    @ParameterizedTest(name = "Test resilience of RKN to active power perturbation on HU networks: {0}")
-    @MethodSource("com.powsybl.openloadflow.knitro.solver.NetworkProviders#provideNodeBreakerHUNetworks")
-    void testVoltagePerturbationOnHUData(NetworkPair pair) {
-        String baseFilename = pair.baseFilename();
-
-        Network rknNetwork = pair.rknNetwork();
-        Network nrNetwork = pair.nrNetwork();
-        Network dcNetwork = pair.dcNetwork();
-
-        // Line Characteristics in per-unit
-        double rPU = 0.0;
-        double xPU = 1e-5;
-        // Voltage Mismatch
-        double alpha = 0.95;
-
-        voltagePerturbationTest(rknNetwork, nrNetwork, dcNetwork, baseFilename, rPU, xPU, alpha);
-    }
-
-    @Test
-    @MethodSource("com.powsybl.openloadflow.knitro.solver.NetworkProviders.ES_INSTANCE")
-    void testVoltagePerturbationOnESData() {
-        Path fileName = Path.of(CONFIDENTIAL_DATA_DIR, ES_INSTANCE);
-        Network nrNetwork = Network.read(fileName).getNetwork();
-        Network rknNetwork = Network.read(fileName).getNetwork();
-        Network dcNetwork = Network.read(fileName).getNetwork();
-
-        // Line Characteristics in per-unit
-        double rPU = 0.0;
-        double xPU = 1e-5;
-        // Voltage Mismatch
-        double alpha = 0.95;
-
-        voltagePerturbationTest(rknNetwork, nrNetwork, dcNetwork, "ES", rPU, xPU, alpha);
     }
 
     @ParameterizedTest(name = "Test resilience of RKN to a voltage perturbation on RTE networks: {0}")
@@ -270,45 +205,6 @@ public class ResilientAcLoadFlowPerturbationTest {
         activePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, baseFilename, alpha);
     }
 
-    @Test
-    void testActivePowerPerturbationOnHUInstance() {
-        Path fileName = Path.of(CONFIDENTIAL_DATA_DIR + "HU_nb/", HU_INSTANCE);
-        Network nrNetwork = Network.read(fileName).getNetwork();
-        Network rknNetwork = Network.read(fileName).getNetwork();
-        Network dcNetwork = Network.read(fileName).getNetwork();
-
-        // Final perturbed load's percentage
-        double alpha = 0.30;
-
-        activePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, "HU", alpha);
-    }
-
-    @ParameterizedTest(name = "Test resilience of RKN to active power perturbation on HU networks: {0}")
-    @MethodSource("com.powsybl.openloadflow.knitro.solver.NetworkProviders#provideNodeBreakerHUNetworks")
-    void testActivePowerPerturbationOnHUData(NetworkPair pair) {
-        Network rknNetwork = pair.rknNetwork();
-        Network nrNetwork = pair.nrNetwork();
-        Network dcNetwork = pair.dcNetwork();
-
-        // Final perturbed load's percentage
-        double alpha = 0.30;
-
-        activePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, null, alpha);
-    }
-
-    @Test
-    void testActivePowerPerturbationOnESData() {
-        Path fileName = Path.of(CONFIDENTIAL_DATA_DIR, ES_INSTANCE);
-        Network nrNetwork = Network.read(fileName).getNetwork();
-        Network rknNetwork = Network.read(fileName).getNetwork();
-        Network dcNetwork = Network.read(fileName).getNetwork();
-
-        // Final perturbed load's percentage
-        double alpha = 0.10;
-
-        activePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, "ES", alpha);
-    }
-
     @ParameterizedTest(name = "Test resilience of RKN to active power perturbation on RTE networks: {0}")
     @MethodSource("com.powsybl.openloadflow.knitro.solver.NetworkProviders#provideRteNetworks")
     void testActivePowerPerturbationOnRteNetworks(NetworkPair pair) {
@@ -322,46 +218,6 @@ public class ResilientAcLoadFlowPerturbationTest {
         double alpha = 0.10;
 
         activePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, baseFilename, alpha);
-    }
-
-    @Test
-    void testReactivePowerPerturbationOnHUInstance() {
-        Path fileName = Path.of(CONFIDENTIAL_DATA_DIR + "HU_bb/", HU_INSTANCE);
-        Network nrNetwork = Network.read(fileName).getNetwork();
-        Network rknNetwork = Network.read(fileName).getNetwork();
-        Network dcNetwork = Network.read(fileName).getNetwork();
-        // Target reactive power injection by the shunt section in VArs
-        double targetQ = 1e9;
-
-        reactivePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, "HU", targetQ);
-    }
-
-    @ParameterizedTest(name = "Test resilience of RKN to reactive power perturbation on HU networks: {0}")
-    @MethodSource("com.powsybl.openloadflow.knitro.solver.NetworkProviders#provideBusBreakerHUNetworks")
-    void testReactivePowerPerturbationOnHUData(NetworkPair pair) {
-        String baseFilename = pair.baseFilename();
-
-        Network rknNetwork = pair.rknNetwork();
-        Network nrNetwork = pair.nrNetwork();
-        Network dcNetwork = pair.dcNetwork();
-
-        // Target reactive power injection by the shunt section in VArs
-        double targetQ = 1e9;
-
-        reactivePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, baseFilename, targetQ);
-    }
-
-    @Test
-    void testReactivePowerPerturbationOnESData() {
-        Path fileName = Path.of(CONFIDENTIAL_DATA_DIR, ES_INSTANCE);
-        Network nrNetwork = Network.read(fileName).getNetwork();
-        Network rknNetwork = Network.read(fileName).getNetwork();
-        Network dcNetwork = Network.read(fileName).getNetwork();
-
-        // Target reactive power injection by the shunt section in VArs
-        double targetQ = 5e9;
-
-        reactivePowerPerturbationTest(rknNetwork, nrNetwork, dcNetwork, "ES", targetQ);
     }
 
     @ParameterizedTest(name = "Test resilience of RKN to reactive power perturbation on RTE networks: {0}")
