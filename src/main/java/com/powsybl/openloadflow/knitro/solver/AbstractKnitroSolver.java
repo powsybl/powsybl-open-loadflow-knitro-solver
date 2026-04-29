82e076d56e94e811ab30c91c093e6439c6c6e293
Salomé Lavine <salome.lavine@artelys.com>
2026-04-29 14:18:31 +0200

    Fix (undo angle convertion) in ResilientAcLoadFlowUnitTest
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>
    
    Add ( DC Losses computation, code not clean ) in ResilientAcLoadFlowPerturbationTest
    
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>
    
    FIX (Parameters) fix corrupted channel error in surefire-reports during `mvn test` by setting KN_PARAM_OUTLEV to zero
    
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>
    
    FIX (Parameters) fix corrupted channel error in surefire-reports during `mvn test` by setting KN_PARAM_OUTLEV to zero
    
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>

--- src/main/java/com/powsybl/openloadflow/knitro/solver/AbstractKnitroSolver.java
+++ src/main/java/com/powsybl/openloadflow/knitro/solver/AbstractKnitroSolver.java
@@ -79,6 +79,7 @@ public abstract class AbstractKnitroSolver extends AbstractAcSolver {
         solver.setParam(KNConstants.KN_PARAM_HESSOPT, knitroParameters.getHessianComputationMode());
         solver.setParam(KNConstants.KN_PARAM_SOLTYPE, KNConstants.KN_SOLTYPE_BESTFEAS);
         solver.setParam(KNConstants.KN_PARAM_OUTLEV, 3);
+        solver.setParam(KNConstants.KN_PARAM_OUTMODE, 1);
         solver.setParam(KNConstants.KN_PARAM_NUMTHREADS, knitroParameters.getThreadNumber());
 
         LOGGER.info("Knitro parameters set: GRADOPT={}, HESSOPT={}, FEASTOL={}, OPTTOL={}, MAXIT={}",
