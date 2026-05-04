04bb3cfe8c51bfedaf2490e7c82956e20b3d882f
Salomé Lavine <salome.lavine@artelys.com>
2026-05-04 17:16:29 +0200

    Fix (checkstyle and line removed and commented unsued import) in KnitroSolverParameters, ResilientAcLoadFlowPerturbationTest
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>

--- src/main/java/com/powsybl/openloadflow/knitro/solver/KnitroSolverParameters.java
+++ src/main/java/com/powsybl/openloadflow/knitro/solver/KnitroSolverParameters.java
@@ -37,7 +37,6 @@ public class KnitroSolverParameters implements AcSolverParameters {
     public static final boolean ALWAYS_UPDATE_NETWORK_DEFAULT_VALUE = false;
     public static final SolverType DEFAULT_SOLVER_TYPE = SolverType.STANDARD;
     public static final int DEFAULT_THREAD_NUMBER = -1;
-    //public static final double DEFAULT_OMEGA_P1 = 1.0; //
     public static final double DEFAULT_DC_LOSSES = 10.0; // MW
 
     private StateVectorScalingMode stateVectorScalingMode = DEFAULT_STATE_VECTOR_SCALING_MODE;
@@ -77,7 +76,6 @@ public class KnitroSolverParameters implements AcSolverParameters {
     private SolverType solverType = DEFAULT_SOLVER_TYPE;
 
     private int threadNumber = DEFAULT_THREAD_NUMBER; // Specifies the number of threads used by the solver. -1 lets the solver decide
-
 
     private double losses = DEFAULT_DC_LOSSES;
 
