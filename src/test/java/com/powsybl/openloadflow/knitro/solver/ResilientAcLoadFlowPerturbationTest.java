4c1621af195e592f34f99349720857a97ccb6b7e
Salomé Lavine <salome.lavine@artelys.com>
2026-05-04 17:20:39 +0200

    Fix (commented unsued import) in ResilientAcLoadFlowPerturbationTest
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>

--- src/test/java/com/powsybl/openloadflow/knitro/solver/ResilientAcLoadFlowPerturbationTest.java
+++ src/test/java/com/powsybl/openloadflow/knitro/solver/ResilientAcLoadFlowPerturbationTest.java
@@ -1,7 +1,7 @@
 package com.powsybl.openloadflow.knitro.solver;
 
 //import com.powsybl.commons.report.ReportNode;
-import com.powsybl.iidm.network.Bus;
+//import com.powsybl.iidm.network.Bus;
 import com.powsybl.iidm.network.Line;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.Terminal;
@@ -19,7 +19,7 @@ import com.powsybl.openloadflow.OpenLoadFlowProvider;
 import com.powsybl.openloadflow.network.*;
 //import com.powsybl.openloadflow.network.impl.LfNetworkLoaderImpl;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertTrue;
