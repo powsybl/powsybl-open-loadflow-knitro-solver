9e134b4fc7c85762f9e464ff5f35907d25c54e3e
Salomé Lavine <salome.lavine@artelys.com>
2026-06-01 16:11:03 +0200

    Refactor in ResilientAcLoadFlowPerturbationTest
    Signed-off-by: Salomé Lavine <salome.lavine@artelys.com>

--- src/test/java/com/powsybl/openloadflow/knitro/solver/ResilientAcLoadFlowPerturbationTest.java
+++ src/test/java/com/powsybl/openloadflow/knitro/solver/ResilientAcLoadFlowPerturbationTest.java
@@ -38,7 +38,6 @@ class ResilientAcLoadFlowPerturbationTest {
     private static final String RKN = "KNITRO";
     private static final String NR = "NEWTON_RAPHSON";
     private static final String VOLTAGE_PERTURBATION = "voltage-perturbation";
-    private static final String ACTIVE_POWER_PERTURBATION = "P_perturbation";
     private static final String EXPORT_CSV = "Slack_info/";
     private static final boolean EXPORT = true;
     private LoadFlow.Runner loadFlowRunner;
